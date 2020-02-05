d87ef3b623129b51e5e55bd432a9806bf8fb942c
Bryan Cantz <bryancantz@comcast.net>
2020-02-05 10:55:55 -0500

    Update newrelic.js

--- newrelic.js
+++ newrelic.js
@@ -13,7 +13,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key: '097c93a634574b826b4565b687fca97dcf5309eb',
+  license_key: '--',
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
